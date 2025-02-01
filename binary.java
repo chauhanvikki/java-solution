63757a703bec5a0c73a376f329b7cd521b575168
Vishvendra SIngh <112617024+chauhanvikki@users.noreply.github.com>
2025-02-01 17:55:32 +0530

    binary.java

--- binary.java
+++ binary.java
@@ -1,81 +1,81 @@
-// public class binary{
-//     public static int binary(int arr[],int target){
-//         int start=0;
-//         int end=arr.length-1;
-//         // int mid=start-(start+end)/2;
-//         while(start<=end){
-//             int mid=start+(end-start)/2;
-//             if(arr[mid]==target){
-//                 // System.out.print(mid);
-//                 return mid;
-//             }
-//             if(arr[mid]<target){
-//                 start=mid+1;
-//             }
-//             else{
-//                 end=mid-1;
-//             }
-//         }
-//         return -1;
-//     }
-//     public static void main(String args[]){
-//         int arr[]={1,23,34,54,65,76,677};
-//         int target=65;
-//         int result=binary(arr,target);
-//         if(result!=-1){
-//             System.out.println("Element found at index : "+result);
-//         }
-//         else{
-//             System.out.println("Index not found: Error!!!");
-//         }
-//     }
-// }
+public class binary{
+    public static int binary(int arr[],int target){
+        int start=0;
+        int end=arr.length-1;
+        // int mid=start-(start+end)/2;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                // System.out.print(mid);
+                return mid;
+            }
+            if(arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int arr[]={1,23,34,54,65,76,677};
+        int target=65;
+        int result=binary(arr,target);
+        if(result!=-1){
+            System.out.println("Element found at index : "+result);
+        }
+        else{
+            System.out.println("Index not found: Error!!!");
+        }
+    }
+}
 // ==========================================================================================================
 
-// when array in decsending or ascending order(Code for Order-agnostic Binary Search)
-// public class binary{
-//     public static int binary(int arr[],int target){
-//         int start=0;
-//         int end=arr.length-1;
-//         boolean asc=arr[start]<arr[end];
-//         // int mid=start-(start+end)/2;
-//         while(start<=end){
-//             int mid=start+(end-start)/2;
-//             if(arr[mid]==target){
-//                 // System.out.print(mid);
-//                 return mid;
-//             }
-//             if(asc){
-//                 if(arr[mid]<target){
-//                 start=mid+1;
-//             }
-//             else{
-//                 end=mid-1;
-//             }
-//             }
-//             else{
-//                 if(arr[mid]>target){
-//                 start=mid+1;
-//             }
-//             else{
-//                 end=mid-1;
-//             }
-//             }
-//         }
-//         return -1;
-//     }
-//     public static void main(String args[]){
-//         int arr[]={233,33,12,2,1};
-//         int target=2;
-//         int result=binary(arr,target);
-//         if(result!=-1){
-//             System.out.println("Element found at index : "+result);
-//         }
-//         else{
-//             System.out.println("Index not found: Error!!!");
-//         }
-//     }
-// }
+when array in decsending or ascending order(Code for Order-agnostic Binary Search)
+public class binary{
+    public static int binary(int arr[],int target){
+        int start=0;
+        int end=arr.length-1;
+        boolean asc=arr[start]<arr[end];
+        // int mid=start-(start+end)/2;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                // System.out.print(mid);
+                return mid;
+            }
+            if(asc){
+                if(arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            }
+            else{
+                if(arr[mid]>target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int arr[]={233,33,12,2,1};
+        int target=2;
+        int result=binary(arr,target);
+        if(result!=-1){
+            System.out.println("Element found at index : "+result);
+        }
+        else{
+            System.out.println("Index not found: Error!!!");
+        }
+    }
+}
 // =============================================================================================================
 // binary search with recursion
 public class binary{
